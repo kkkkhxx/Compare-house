a1b33c1cbeed687b3d842792dcd65fac0af48f85
KKKKHXX <119933864+kkkkhxx@users.noreply.github.com>
2025-06-17 15:23:49 +0700

    Update loadFooter.js

--- component/loadFooter.js
+++ component/loadFooter.js
@@ -2,7 +2,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     const footer = document.querySelector("footer");
     if (footer) {
-        fetch("/component/footer.html") // ปรับ path ให้สัมพันธ์กับหน้าแต่ละหน้า
+        fetch("component/footer.html") // ปรับ path ให้สัมพันธ์กับหน้าแต่ละหน้า
             .then(res => res.text())
             .then(html => {
                 footer.innerHTML = html;
