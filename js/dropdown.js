7918fbcee8cc285a91eaf3f5bb3e42fa018325ca
KKKKHXX <119933864+kkkkhxx@users.noreply.github.com>
2025-06-18 09:59:33 +0700

    Update dropdown.js

--- js/dropdown.js
+++ js/dropdown.js
@@ -38,7 +38,7 @@ function renderFormGroups() {
                 row = document.createElement('div');
                 row.className = 'form-row';
                 row.innerHTML = `<div class="icon">
-                    <img src="../assets/icon/${opt.folder}/${opt.icon}" alt="${opt.label}"></div>
+                    <img src="assets/icon/${opt.folder}/${opt.icon}" alt="${opt.label}"></div>
                     <label>${opt.label}</label>
                     <input type="text" name="${opt.name}" placeholder="กรอกข้อมูล">`;
             }
@@ -49,7 +49,7 @@ function renderFormGroups() {
                 const iconDiv = document.createElement('div');
                 iconDiv.className = 'icon';
                 const icon = document.createElement('img');
-                icon.src = `../assets/icon/${opt.folder}/${opt.icon}`;
+                icon.src = `assets/icon/${opt.folder}/${opt.icon}`;
                 icon.alt = opt.label;
                 iconDiv.appendChild(icon);
 
@@ -79,7 +79,7 @@ function renderFormGroups() {
             }
             else {
                 const clone = template.content.cloneNode(true);
-                clone.querySelector('img').src = `../assets/icon/${opt.folder}/${opt.icon}`;
+                clone.querySelector('img').src = `assets/icon/${opt.folder}/${opt.icon}`;
                 clone.querySelector('img').alt = opt.label;
                 clone.querySelector('label').textContent = opt.label;
                 clone.querySelector('input[type="checkbox"]').name = opt.name;
@@ -199,7 +199,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
             const preview = document.getElementById("image_preview");
             if (preview && home.imageName) {
-                preview.src = `../assets/photo/${home.imageName}`;
+                preview.src = `assets/photo/${home.imageName}`;
                 preview.style.display = "block";
             }
         }
